bfaf9c16e6b07718de9cee93c2841d58539b2763
Emanuel Alessi <emanuelalessi14@gmail.com>
2025-08-11 13:01:41 -0300

    Update firebase.ts

--- taller-mecanico/src/lib/firebase.ts
+++ taller-mecanico/src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC8DpDcgvvVUWl7O_vMCtOgCKpDx_73Qvs",
+  authDomain: "gtm-sn.firebaseapp.com",
+  projectId: "gtm-sn",
+  storageBucket: "gtm-sn.appspot.com",
+  messagingSenderId: "931181938365",
+  appId: "1:931181938365:web:572bdf4b54dfafe67599d4",
 }
 
 // Log simple para verificar que todo funciona

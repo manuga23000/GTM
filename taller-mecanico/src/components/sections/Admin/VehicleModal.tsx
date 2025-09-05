b363f22c5eb444bd150f041131c60abf58121c69
Emanuel Alessi <emanuelalessi14@gmail.com>
2025-09-05 09:56:36 -0300

    Update VehicleModal.tsx

--- taller-mecanico/src/components/sections/Admin/VehicleModal.tsx
+++ taller-mecanico/src/components/sections/Admin/VehicleModal.tsx
@@ -1040,7 +1040,7 @@ export default function VehicleModal({
                   </button>
                 </div>
 
-                {datosHistorialCargados && (
+                {_datosHistorialCargados && (
                   <motion.div
                     initial={{ opacity: 0, y: -10 }}
                     animate={{ opacity: 1, y: 0 }}
